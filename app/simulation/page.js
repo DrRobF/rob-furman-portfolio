f2ac014e04845d12da6eef0a35e32c5cc3ea924d
DrRobF <drrobfurman@gmail.com>
2026-04-24 11:08:34 -0400

    Add interactive school leader simulation shell page

---
+++ app/simulation/page.js
@@ -0,0 +1,11 @@
+import SimulationShellClient from './SimulationShellClient';
+
+export default function SimulationPage() {
+  return (
+    <section className="section section-light">
+      <div className="container">
+        <SimulationShellClient />
+      </div>
+    </section>
+  );
+}
